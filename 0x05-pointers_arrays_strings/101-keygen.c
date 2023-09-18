009508725d69b1cf477f23bcbee03dc53c1f59b5
CollinsMwambi <collins.mwambi@strathmore.edu>
2023-09-18 10:07:23 -0700

    finaltask

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,39 +1,37 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
- * generatePassword - Generates a random password
- * @password: Pointer to the password buffer
- * @length: Length of the password
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
-void generatePassword(char *password, int length) {
-    /* Define the characters allowed in the password */
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
 
-    /* Seed the random number generator with the current time */
-    srand((unsigned int)time(NULL));
+	sum = 0;	
 
-    for (int i = 0; i < length; i++) {
-        /* Generate a random index to select a character from the charset */
-        int index = rand() % (sizeof(charset) - 1);
+	srand(time(NULL));
 
-        /* Append the selected character to the password */
-        password[i] = charset[index];
-    }
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
-    /* Null-terminate the password string */
-    password[length] = '\0';
-}
-
-int main() {
-    int passwordLength = 12; /* Change this to match your desired password length */
-    char password[passwordLength + 1]; /* +1 for the null terminator */
-
-    generatePassword(password, passwordLength);
-
-    printf("Generated Password: %s\n", password);
-
-    return 0;
+	return (0);
 }
 
