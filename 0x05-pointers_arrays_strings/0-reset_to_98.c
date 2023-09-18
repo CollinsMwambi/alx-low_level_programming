a87b850acd86060918c6493593b653fd920b5f7b
CollinsMwambi <collins.mwambi@strathmore.edu>
2023-09-18 08:31:37 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * reset_to_98 - resets to a value 98
+ * @n - pointer to the value to be changed
+ * Return: void
+ *
+ *
+ *
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
+
