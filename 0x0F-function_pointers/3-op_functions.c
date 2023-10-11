dbae754953ebd971a7fff3141a4fc95ead4d80c6
CollinsMwambi <collins.mwambi@strathmore.edu>
2023-10-11 07:34:32 -0700

    correction

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,40 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <udis86.h>
 
 /**
-  * op_add - ...
-  * @a: ...
-  * @b: ...
+  * main - ...
+  * @argc: ...
+  * @argv: ...
   *
   * Return: ...
   */
-int op_add(int a, int b)
+int main(int argc, char *argv[])
 {
-	return (a + b);
-}
+	ud_t ud_obj;
+	int val = 0, i = 0;
 
-/**
-  * op_sub - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
-  * op_mul - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
-  * op_div - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_div(int a, int b)
-{
-	if (b == 0)
+	if (argc == 2)
 	{
-		printf("Error\n");
-		exit(100);
+		val = atoi(argv[1]);
+
+		if (val < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+		ud_unit(&ud_obj);
+		ud_set_input_buffer(&ud_obj, argv[1], val);
+		ud_set_mode(&ud_obj, 64);
+		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+		while (ud_disassemble(&ud_obj))
+		{
+			printf("\t%s\n", ud_insn_hex(&ud_obj));
+		}
 	}
 
-	return (a / b);
+	return (0);
 }
 
-/**
-  * op_mod - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a % b);
-}
